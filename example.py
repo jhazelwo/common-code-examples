7cd9726bec18500c9de7032a5097673587d583ae
John Hazelwood <jhazelwo@users.noreply.github.com>
2016-12-23 13:51:01 -0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Import a module
+import modulename
+
+# Define a variable
+foo = 'bar'
+
+# Print "hello world"
+print('hello world')
+
+# If, then, else
+if 1 == 1:
+  pass
+elif 2 == 2:
+  pass
+else:
+  pass
+
+# Iterate
+for i in range(99):
+    pass
+
+# Define a function
+def baz(a):
+  pass
+
+# Run a fucntion
+baz('bar')
+
+# Define a class
+class Bob(object):
+  pass
+
+# Instantiate a class
+tom = Bob()
+
