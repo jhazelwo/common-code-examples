c368dec08e0f0a9fcf754491a2d756a94219f410
John Hazelwood <jhazelwo@users.noreply.github.com>
2016-12-23 14:03:57 -0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,43 @@
+<?php
+
+// Import a module
+include modulename;
+
+// Define a variable
+$foo = 'bar';
+
+// Print "hello world"
+echo 'hello world');
+
+// If, then, else
+if (1 == 1) {
+    echo '';
+} elseif (2 == 2) {
+    echo '';
+} else {
+    echo '';
+}
+
+// Iterate
+foreach ($array as &$value) {
+    echo '';
+}
+
+// Define a function
+function foo(arg)
+{
+    echo $arg;
+}
+
+// Run a fucntion
+foo('bar')
+
+// Define a class
+class Bob {
+    public $var = 1;
+}
+
+// Instantiate a class
+$tom = new Bob();
+
+?>
